4d135373537504fa6dd1893e92871019efe45bb4
Jason Tan <jasontan@Jasons-MBP.lan>
2024-02-28 01:45:23 -0500

    image wall

---
+++ src/app/images/page.tsx
@@ -0,0 +1,13 @@
+export default function Images() {
+    let ids: number[] = []
+    for (var i=0; i < 39; i++) {
+        ids.push(i + 3)
+    }
+    return (
+        <main className="flex min-h-screen grid grid-cols-3 gap-4 p-24">
+            {ids.map((id) => {
+                return <div><img className="object-fill w-full h-full" key={id} src={`https://picsum.photos/id/${id}/200/300`}></img></div>
+            })}
+        </main>
+    )
+}
