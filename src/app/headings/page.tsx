c7871a25ba92fd06d45da60b5bea094d346df4de
Christian Rodriguez Ochoa <christian@testparty.ai>
2024-05-03 11:19:44 -0400

    created tester headings page

---
+++ src/app/headings/page.tsx
@@ -0,0 +1,24 @@
+export default function Headings() {
+    return (
+        <div>
+            <h1>Heading 1.</h1>
+            <h2>Heading 2.</h2>
+            <p>This is a paragraph.</p>
+
+            <h2>Heading 2..</h2>
+            <h4>Heading 4.</h4>
+            <h5>Heading 5.</h5>
+            <h6>Heading 6.</h6>
+            <p>This is a paragraph..</p>
+
+            <div>
+                <h4>Heading 4..</h4>
+                <h6>Heading 6..</h6>
+                <p>This is a paragraph...</p>
+            </div>
+            
+            <h2>Heading 2...</h2>
+            <p>This is a paragraph...</p>
+        </div>
+    );
+}
