7644e2da03ce1e426305825d529c7f1ee0975fde
Alexander Kronfeld <alexander@testparty.ai>
2024-10-15 14:57:56 -0400

    amk - adding test cases

--- src/app/aria-required-parent/page.tsx
+++ src/app/aria-required-parent/page.tsx
@@ -6,16 +6,32 @@ export default function Page() {
   return (
     <body>
       <ul id="myaccount">
-        <li role="menuitem">
-          Bacon
-        </li>
-        <li role="menuitem">
-          Egg
-        </li>
-        <li role="menuitem">
-          Cheese
-        </li>
+        <li><div role="listitem">Bacon</div></li>
+        <li><div role="listitem">Egg</div></li>
+        <li><div role="listitem">Cheese</div></li>
       </ul>
+
+      <ul id="myaccount">
+        <li><div role="menuitem">Foo</div></li>
+      </ul>
+
+      <table>
+        <tr>
+          <th>Header 1</th>
+          <th>Header 2</th>
+          <th>Header 3</th>
+        </tr>
+        <tr>
+          <td><div role='cell'>Row 1, Cell 1</div></td>
+          <td><div>Row 1, Cell 2</div></td>
+          <td><div>Row 1, Cell 3</div></td>
+        </tr>
+        <tr> 
+          <td>Row 2, Cell 1</td>
+          <td>Row 2, Cell 2</td>
+          <td>Row 2, Cell 3</td>
+        </tr>
+      </table>
     </body>
   );
 }
