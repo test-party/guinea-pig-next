d5fa83de0dd59383e1edb2af9838c7c2fa21efcd
Alexander Kronfeld <alexander@testparty.ai>
2024-10-17 18:04:48 -0400

    amk - cascading list test case

--- src/app/aria-required-parent/page.tsx
+++ src/app/aria-required-parent/page.tsx
@@ -2,32 +2,14 @@
 export default function Page() {
   return (
     <body>
-      {/* <table>
-        <tr>
-        <li role="none"><div role="listitem">Bacon</div></li>  <th>Header 1</th>
-        <li role="none"><div role="listitem">Egg</div></li>  <th>Header 2</th>
-        <li role="none"><div role="listitem">Cheese</div></li>  <th>Header 3</th>
-        </tr>
-        <tr>
-          <td><div role='cell'>Row 1, Cell 1</div></td>
-          <td><div>Row 1, Cell 2</div></td>
-          <td><div>Row 1, Cell 3</div></td>
-        </tr>
-        <tr> 
-          <td>Row 2, Cell 1</td>
-          <td>Row 2, Cell 2</td>
-          <td>Row 2, Cell 3</td>
-        </tr>
-      </table> */}
-
       <div>
-        <div role="row">
+        <div>
           <div role="cell">This is a Cell</div>
         </div>
       </div>
 
       <div>
-        <div>
+        <div role="row">
           <div role="cell">This is a Cell with row</div>
         </div>
       </div>
@@ -35,7 +17,7 @@ export default function Page() {
       <div>
         <div role="rowgroup">
           <div>
-            <div role="cell">This is a Cell with row</div>
+            <div role="cell">This is a Cell with rowgroup</div>
           </div>
         </div>
       </div>
