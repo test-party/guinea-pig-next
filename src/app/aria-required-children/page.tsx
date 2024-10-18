a13ad75ca39a56b10e0fc53dd9df09ab54e17f68
felipe-testparty <felipe@testparty.ai>
2024-10-18 14:50:54 -0300

    aria-required-children

---
+++ src/app/aria-required-children/page.tsx
@@ -0,0 +1,15 @@
+const Page = () => {
+    return (
+        <div id="a" role="table">
+            <div id="b">
+                <div id="c">CELL</div>
+            </div>
+            <div id="d">
+                <div id="e">CELL</div>
+            </div>
+        </div>
+    );
+};
+
+export default Page;
+//http://localhost:3000/aria-required-children
