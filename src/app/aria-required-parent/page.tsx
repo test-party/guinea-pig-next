5c8abeb3537080e5f2d6d1c3b7a52aa27def7d02
Alexander Kronfeld <alexander@testparty.ai>
2024-10-18 10:14:06 -0400

    amk - improved test suite

--- src/app/aria-required-parent/page.tsx
+++ src/app/aria-required-parent/page.tsx
@@ -4,37 +4,63 @@ export default function Page() {
     <body>
       <div>
         <div>
-          <div role="cell">This is a Cell</div>
+          <div role="cell">This Cell needs row and row's parent options</div>
         </div>
       </div>
 
       <div>
         <div role="row">
-          <div role="cell">This is a Cell with row</div>
+          <div role="cell">This Cell has row, needs row's parent options</div>
         </div>
       </div>
 
       <div>
         <div role="rowgroup">
           <div>
-            <div role="cell">This is a Cell with rowgroup</div>
+            <div role="cell">This Cell has rowgroup, needs row in between and rowgroup's parent options</div>
           </div>
         </div>
       </div>
 
       <div>
         <div>
-          <div role="cell">This is a Cell in row 1</div>
+          <div role="cell">This is Cell 1, it needs row and row's parent options</div>
         </div>
         <div>
-          <div>This is supposed to be a Cell in row 2</div>
+          <div>This is supposed to be cell under row.  It should be remediated as part of Cell 1's remediation</div>
         </div>
       </div>
 
       <div>
-        <div role="listitem">Bacon</div>
-        <div>Egg</div>
-        <div>Cheese</div>
+        <div role="caption">caption needs parent options</div>
+      </div>
+
+      <div>
+        <div role="treeitem">treeitem needs parent options</div>
+        <div>Should be treeitem</div>
+      </div>
+
+      <div>
+        <div role="option">option needs parent options</div>
+        <div>Should be Option</div>
+      </div>
+
+      <div>
+        <div role="menuitem">menuitem needs parent options</div>
+        <div>Should be menuitem</div>
+        <div>Should be menuitem</div>
+      </div>
+
+      <div>
+        <div role="listitem">listitem needs parent list</div>
+        <div>Should be listitem</div>
+        <div>Should be listitem</div>
+      </div>
+
+      <div>
+        <div role="tab">tab needs parent tablist</div>
+        <div>Should be tab</div>
+        <div>Should be tab</div>
       </div>
     </body>
   );
