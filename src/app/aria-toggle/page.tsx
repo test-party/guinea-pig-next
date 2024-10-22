11dad231a82bab63d5b1a11c31c22e8276e3be7b
felipe-testparty <felipe@testparty.ai>
2024-10-22 12:31:41 -0300

    add test case aria-toggle-field

---
+++ src/app/aria-toggle/page.tsx
@@ -0,0 +1,30 @@
+const Page = () => {
+    return (
+        <div>
+            <ul role="menu">
+                <li id="pass2" role="menuitemcheckbox" aria-checked="true">
+                </li>
+            </ul>
+            <p id="pass3Label">Sans-serif</p>
+            <ul role="menu">
+                <li id="pass3" role="menuitemradio" aria-checked="true">
+                </li>
+                <li id="pass3" role="menuitemradio" aria-checked="true">
+                </li>
+            </ul>
+            <div role="radiogroup">
+                <div id="pass4" role="radio" aria-checked="false">
+                </div>
+                <div id="pass4" role="radio" aria-checked="false">
+                </div>
+            </div>
+            <div id="fail5" role="switch" aria-checked="true">
+                <span></span>
+                <span></span>
+            </div>
+        </div>);
+};
+
+export default Page;
+
+// http://localhost:3000/aria-toggle
