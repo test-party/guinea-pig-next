6a9f4f4858c8dca4b6032be725375edf19e4084b
Artur Meinen <arturmeinen@gmail.com>
2024-10-23 16:40:01 -0300

    new aria-valid tests

--- src/app/aria-valid-attr/page.tsx
+++ src/app/aria-valid-attr/page.tsx
@@ -5,7 +5,22 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Page() {
   return (
     <body className={inter.className}> 
-      <div aria-xxxx="xxzcxzc" ></div>
+      <button aria-lab="fdasdasdasalse"></button>
+      <button aria-lab="false"></button>
+      <button aria-hidd="false"></button>
+      <button aria-xxxx="sadddas"></button>
+      <button aria-yyyy="false"></button>
+      <button aria-tesxt="1"></button>
+      <button aria-false="false"></button>
+      <button aria-11111={1}></button>
+      <button aria-invalid="true"></button>
+      <button aria-busy="true"></button>
+      <button aria-checked="mixed"></button>
+      <button aria-expad="true"></button>
+      <button aria-hapup="menu"></button>
+      <button aria-hiden="true"></button>
+      <button aria-lal="Submit"></button>
+      <button aria-s="assertive"></button>
     </body>
   );
 } 
