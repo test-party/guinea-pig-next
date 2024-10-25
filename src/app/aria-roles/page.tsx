a175243089bce7982e89cb093a828c205fdff817
felipe-testparty <felipe@testparty.ai>
2024-10-25 15:46:25 -0300

    aria-roles

---
+++ src/app/aria-roles/page.tsx
@@ -0,0 +1,20 @@
+const Page = () => {
+    return (
+        <>
+            <div role="invalid-name">
+            </div>
+            <ul role="invalid-name">
+                <li role="invalid-name">
+                </li>
+            </ul>
+            <div role="invalid-name">
+            </div>
+            <span role="invalid-name">
+            </span>
+        </>
+    );
+};
+
+export default Page;
+
+// http://localhost:3000/aria-roles
