d7f67c07f99406a8e953fef0ea93d7390955e69b
Alexander Kronfeld <alexander@testparty.ai>
2024-11-12 11:09:43 -0500

    amk - update format

--- src/app/aria-braille-equivalent/page.tsx
+++ src/app/aria-braille-equivalent/page.tsx
@@ -2,19 +2,12 @@
 export default function Page() {
     return (
       <body>
-        <div
-          role="article"
-          id="slide"
-          aria-brailleroledescription="slide">BACON</div>
-        <div
-          role="article"
-          id="slide"
-          aria-braillelabel="****">EGG</div>
-        <div
-          role="article"
-          id="slide"
-          aria-braillelabel="****"
-          aria-brailleroledescription="slide">CHEESE</div>
+        <div role="article" id="slide" aria-brailleroledescription="slide">BACON</div>
+        <div role="article" id="slide" aria-braillelabel="****">EGG</div>
+        <div role="article" id="slide" aria-braillelabel="****" aria-brailleroledescription="slide">CHEESE</div>
+        <div role="article" id="slide" aria-labelledby="slideheading" aria-brailleroledescription="slide">
+          <h1 id="slideheading">My vacation in Rome</h1>
+        </div>
       </body>
     )
   }
