f5596eae1ec25d745578c66833d7ba2b2bd42f55
Artur Meinen <arturmeinen@gmail.com>
2024-11-29 11:37:06 -0300

    add global stress test

---
+++ src/app/global-stress-test/page.tsx
@@ -0,0 +1,15 @@
+//http://localhost:3000/global-stress-test
+export default function GlobalStressTest() {
+    return (
+        <div>
+            <div id="custom-test-global-case-1">INITIAL CODE</div>
+            <div id="custom-test-global-case-2">INITIAL CODE</div>
+            <div id="custom-test-global-case-3">
+                INITIAL CODE
+            </div>
+            <div id="custom-test-global-case-4">
+                INITIAL CODE
+            </div>
+        </div>
+    )
+}
