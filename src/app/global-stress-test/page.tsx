9864f90239a9d124f16a9ea560407866421f32ea
Artur Meinen <arturmeinen@gmail.com>
2024-12-02 22:44:22 -0300

    add global stress test cases

--- src/app/global-stress-test/page.tsx
+++ src/app/global-stress-test/page.tsx
@@ -10,6 +10,26 @@ export default function GlobalStressTest() {
             <div id="custom-test-global-case-4">
                 INITIAL CODE
             </div>
+            <div id="custom-test-global-case-5">INITIAL CODE</div>
+            <div id="custom-test-global-case-6">
+                INITIAL CODE
+            </div>
+            <div><div><div id="custom-test-global-case-7">INITIAL CODE</div></div></div>
+            <div><div><div id="custom-test-global-case-8">INITIAL CODE</div></div></div>
+            <div id="custom-test-global-case-9">
+                <p>children</p>
+                <p>children</p>
+                <p>children</p>
+            </div>
+            <div><div><div id="custom-test-global-case-10">INITIAL CODE</div></div></div>
+            <div><div><div id="custom-test-global-case-11"/></div></div>
+            <div><div><div 
+                id="custom-test-global-case-12"
+            />
+            </div></div>
+            {true && <div id="custom-test-global-case-13">INITIAL CODE</div>}
+            {[1,2,3].map((_,index) => <div id={`custom-test-global-case-14-${index}`}>INITIAL CODE</div>)}
+            <div id="custom-test-global-case-15">INITIAL CODE</div>
         </div>
     )
 }
