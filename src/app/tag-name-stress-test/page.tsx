2a8d29b381a47adf21c6f6bce0b69bb03932dcd9
Artur Meinen <arturmeinen@gmail.com>
2024-12-03 17:26:34 -0300

    add tag tests

---
+++ src/app/tag-name-stress-test/page.tsx
@@ -0,0 +1,15 @@
+//http://localhost:3000/tag-name-stress-test
+export default function GlobalStressTest() {
+    return (
+        <div>
+            <div id='custom-test-tag-name-case-1'>CODE</div>
+            <div id='custom-test-tag-name-case-2'>
+                <div>CODE</div>
+            </div>
+            <div><div><div id='custom-test-tag-name-case-3'>CODE</div></div></div>
+            <div id='custom-test-tag-name-case-4' />
+            {true && <div id='custom-test-tag-name-case-5' />}
+            {[1,2,3].map(item => <div id={`custom-test-tag-name-case-6-${item}`} /> ) }
+        </div>
+    )
+}
