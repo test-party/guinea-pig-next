de62bc03ef1aeba5a44590edec1d1fde68ed90f1
Artur Meinen <arturmeinen@gmail.com>
2024-12-09 23:17:29 -0300

    update aria-valid-attr-value

--- src/app/aria-valid-attr-value/page.tsx
+++ src/app/aria-valid-attr-value/page.tsx
@@ -1,12 +1,24 @@
 import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
-//http://localhost:3000/aria-valid-attr
+//http://localhost:3000/aria-valid-attr-value
 //TESTS
+//Should handle a int type case
+//Should handle a boolean type case
+//Should handle a idref type case
 export default function Page() {
   return (
-    <body className={inter.className}> 
-      <button aria-label="false"> BUTTON 1</button>
+    <body className={inter.className}>
+      <div role="grid" aria-colcount="3">
+        <div role="row">
+          <div role="gridcell">Cell 1</div>
+        </div>
+      </div>
+      <button aria-disabled="true"> BUTTON 1</button>
+      <div id="parent" aria-owns="child1">
+        <div id="child1">Child 1</div>
+      </div>
+      <div role="checkbox" aria-checked="true">Item selecionado</div>
     </body>
   );
 }
