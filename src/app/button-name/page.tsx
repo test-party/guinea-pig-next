dea6962d342afcf2a2316e8a0e4bd62fb399aeef
Artur Meinen <arturmeinen@gmail.com>
2025-01-30 16:35:15 -0300

    add page

---
+++ src/app/button-name/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+//http://localhost:3000/button-name
+const Page = () => {
+    return (
+        <div>
+            <button>Click me</button>
+        </div>
+    );
+};   
+
+export default Page;
