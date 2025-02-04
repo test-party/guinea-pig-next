bca9eda58a5b985a46e8485a88a9c2f01f2f6748
Alexander Kronfeld <alexander@testparty.ai>
2025-02-04 15:40:18 -0500

    amk - test cases modified

--- src/app/meta-viewport/page.tsx
+++ src/app/meta-viewport/page.tsx
@@ -3,9 +3,9 @@ export default function Page() {
     return (
         <div>
             <meta name="viewport" />
-            <meta name="viewport" content="user-scalable='no'" />
-            <meta name="viewport" content="user-scalable='no', maximum-scale=2.6" />
-            <meta name="viewport" content="user-scalable='no', maximum-scale=1" />
+            <meta name="viewport" content="user-scalable=no" />
+            <meta name="viewport" content="user-scalable=no, maximum-scale=2.6" />
+            <meta name="viewport" content="user-scalable=no, maximum-scale=1" />
         </div>
     );
 }
