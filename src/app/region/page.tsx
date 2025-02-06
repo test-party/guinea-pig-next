d44278304e39f424b2c45052e4763c84c3c14818
Alexander Kronfeld <alexander@testparty.ai>
2025-02-06 17:43:34 -0500

    amk - region test

---
+++ src/app/region/page.tsx
@@ -0,0 +1,21 @@
+'use client'
+import React from 'react';
+
+//http://localhost:3000/region
+const Page = () => {
+    return (
+        <div>
+            <div>
+                Hello World
+            </div>
+            <div>
+                Foo Bar Baz
+            </div>
+            <div>
+                Lorum Ipsum
+            </div>
+        </div>
+    );
+};
+
+export default Page;
