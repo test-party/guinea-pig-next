8985030314c48beaeca620bee0c34f1be9739d94
Artur Meinen <arturmeinen@gmail.com>
2025-02-06 21:19:22 -0300

    test

--- src/app/meta-refresh/page.tsx
+++ src/app/meta-refresh/page.tsx
@@ -1,20 +1,20 @@
 //http://localhost:3000/meta-refresh
 export default function Page() {
     return (
-        <>
+        <body>
             <div>
-                <meta http-equiv="refresh" content="10" />
-                <meta http-equiv="refresh" content={(60 * 60 * 20).toString()} />
+                <meta />
+                <meta />
                 <meta http-equiv="content-security-policy" />
                 <meta http-equiv="content-type" />
                 <meta http-equiv="default-style" />
             </div>
             <div>
                 <meta name="viewport" />
-                <meta name="viewport" content="user-scalable=no" />
-                <meta name="viewport" content="user-scalable=no, maximum-scale=2.6" />
-                <meta name="viewport" content="user-scalable=no, maximum-scale=1" />
+                <meta name="viewport" content="" />
+                <meta name="viewport" content="maximum-scale=2.6" />
+                <meta name="viewport" content="maximum-scale=3" />
             </div>
-        </>
+        </body>
     );
 }
