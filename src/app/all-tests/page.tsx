aca6e8c882583a4e049f4273d64acfe52e7adf51
Artur Meinen <arturmeinen@gmail.com>
2025-02-06 21:25:48 -0300

    test

--- src/app/all-tests/page.tsx
+++ src/app/all-tests/page.tsx
@@ -230,8 +230,8 @@ export default function AllTests() {
         <div>Should be tab</div>
       </div>
      <div>
-        <meta http-equiv="refresh" content="10" />
-        <meta http-equiv="refresh" content={(60 * 60 * 20).toString()} />
+        {/* <meta http-equiv="refresh" content="10" />
+        <meta http-equiv="refresh" content={(60 * 60 * 20).toString()} /> */}
         {/* <meta http-equiv="content-security-policy" /> */}
         <meta http-equiv="content-type" />
         <meta http-equiv="default-style" />
