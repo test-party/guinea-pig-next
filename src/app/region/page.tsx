04b555a1c5046902460ff844563f30f296e78b67
Alexander Kronfeld <alexander@testparty.ai>
2025-02-11 14:54:09 -0500

    amk - different regions

--- src/app/region/page.tsx
+++ src/app/region/page.tsx
@@ -5,24 +5,61 @@ import React from 'react';
 const Page = () => {
     return (
         <div>
+            <section>
+                Section Tag
+            </section>
+            <div role='section'>
+                Section Role Div
+            </div>
+            <header>
+                Header Tag
+            </header>
+            <div role='banner'>
+                Header Role Div
+            </div>
+            <nav>
+                Nav Tag
+            </nav>
+            <div role='navigation'>
+                Nav Role Div
+            </div>
+            <main>
+                Main Tag
+            </main>
+            <div role='main'>
+                Main Role Div
+            </div>
+            <footer>
+                Footer Tag
+            </footer>
+            <div role='contentinfo'>
+                Footer Role Div
+            </div>
+            <div role="search">
+                Search Role Div
+            </div>
+            <div role="application">
+                Application Role Div
+            </div>
+            <div role="form">
+                Form Role Div
+            </div>
+            <div role="region">
+                Region Role Div
+            </div>
             <div>
-                <div role='main'>
-                    Hello World
-                </div>
-                <div role="main">
+                <div>
                     <div>
                         <div>
-                            <div>
-                                depth testing
-                            </div>
+                            depth testing
                         </div>
                     </div>
                 </div>
-                <div role="main">
-                    Lorum Ipsum
-                </div>
             </div>
-            <div role="main">
+            <div>
+                Lorum Ipsum
+            </div>
+            <div>
                 another one
             </div>
         </div>
