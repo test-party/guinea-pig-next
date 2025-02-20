59dc3d591676e26e170c23276b3c06079e328547
Alexander Kronfeld <alexander@testparty.ai>
2025-02-20 11:50:12 -0500

    amk - tabs to spaces

--- src/app/region/page.tsx
+++ src/app/region/page.tsx
@@ -3,30 +3,30 @@ import React from 'react';
 
 //http://localhost:3000/region
 const Page = () => {
-	return (
-		<body>
-			<div>
-				<div>
-					Main Role Div
-				</div>
-				<div id='1'>
-					<div>
-						<div>
-							<div>
-								depth testing
-							</div>
-						</div>
-					</div>
-				</div>
-				<div id='2'>
-					Lorum Ipsum
-				</div>
-				<div id='3'>
-					another one
-				</div>
-			</div>
-		</body>
-	);
+    return (
+        <body>
+            <div>
+                <div>
+                    Main Role Div
+                </div>
+                <div id='1'>
+                    <div>
+                        <div>
+                            <div>
+                                depth testing
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div id='2'>
+                    Lorum Ipsum
+                </div>
+                <div id='3'>
+                    another one
+                </div>
+            </div>
+        </body>
+    );
 };
 
 export default Page;
