038c691102ea4d3f20135a70f9c68a4db922eabf
Artur Meinen <arturmeinen@gmail.com>
2025-03-10 12:26:17 -0300

    add quote

--- src/app/neurodiversity-ruleset/unusual-words/page.tsx
+++ src/app/neurodiversity-ruleset/unusual-words/page.tsx
@@ -2,7 +2,7 @@
 export default function LanguageOfParts() {
     return (
         <main className="flex min-h-screen grid grid-cols-3 gap-4 p-24">
-            <p>The wizard's defenestration of the cursed tome startled the entire assembly</p>
+            <p>Her sesquipedalian speech left the audience utterly confused</p>
             <p>His walk through the verdant garden surprised the visitors</p>
             <p>This text is working well , no remediations needed </p>
         </main>
