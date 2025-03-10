0a15ff79661d0b7160a403cf64d667812a8950b0
Artur Meinen <arturmeinen@gmail.com>
2025-03-10 13:03:23 -0300

    add quote

--- src/app/neurodiversity-ruleset/unusual-words/page.tsx
+++ src/app/neurodiversity-ruleset/unusual-words/page.tsx
@@ -2,7 +2,7 @@
 export default function LanguageOfParts() {
     return (
         <main className="flex min-h-screen grid grid-cols-3 gap-4 p-24">
-            <p>He obfuscated it</p>
+            <p>He obfuscated it, now i can't see</p>
             <p>His walk through the verdant garden surprised the visitors</p>
             <p>This text is working well , no remediations needed </p>
         </main>
