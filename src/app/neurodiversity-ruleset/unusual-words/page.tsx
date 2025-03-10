4a2e910d788786f666b14be7b858836b5f60112e
Artur Meinen <arturmeinen@gmail.com>
2025-03-10 12:37:06 -0300

    add quote

--- src/app/neurodiversity-ruleset/unusual-words/page.tsx
+++ src/app/neurodiversity-ruleset/unusual-words/page.tsx
@@ -3,7 +3,7 @@ export default function LanguageOfParts() {
     return (
         <main className="flex min-h-screen grid grid-cols-3 gap-4 p-24">
             <p>The scientist's obfuscation made the explanation hard to understand</p>
-            <p>His walk through the verdant (Green with grass or other rich vegetation.)  garden surprised the visitors</p>
+            <p>His walk through the verdant garden surprised the visitors</p>
             <p>This text is working well , no remediations needed </p>
         </main>
     )
