3dbaf338947679bb4810d6520e8ca948e637b8f1
Artur Meinen <arturmeinen@gmail.com>
2025-03-10 12:20:22 -0300

    initial changes

---
+++ src/app/neurodiversity-ruleset/unusual-words/page.tsx
@@ -0,0 +1,10 @@
+//http://localhost:3000/neurodiversity-ruleset/unusual-words
+export default function LanguageOfParts() {
+    return (
+        <main className="flex min-h-screen grid grid-cols-3 gap-4 p-24">
+            <p>The perspicacious cat quickly avoided the obstacles in its path</p>
+            <p>His walk through the verdant garden surprised the visitors</p>
+            <p>This text is working well , no remediations needed </p>
+        </main>
+    )
+}
