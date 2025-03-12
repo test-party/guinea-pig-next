2418798cc5902d9120f85693f1add4592136bd90
Artur Meinen <arturmeinen@gmail.com>
2025-03-12 12:02:25 -0300

    first test

---
+++ src/app/input-button-name/page.tsx
@@ -0,0 +1,10 @@
+//http://localhost:3000/input-button-name
+export default function Page() {
+    return (
+        <main>
+            <form action="#">
+                <input type="button" id="fail3" />
+            </form>
+        </main>
+    );
+}
