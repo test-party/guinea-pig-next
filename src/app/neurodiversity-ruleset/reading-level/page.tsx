ddb20310b26e21217c386292c5416329e4be6fbe
Artur Meinen <arturmeinen@gmail.com>
2025-03-13 14:39:13 -0300

    add page

---
+++ src/app/neurodiversity-ruleset/reading-level/page.tsx
@@ -0,0 +1,9 @@
+//http://localhost:3000/neurodiversity-ruleset/reading-level
+export default function LanguageOfParts() {
+    return (
+        <main className="flex min-h-screen grid grid-cols-3 gap-4 p-24">
+            <p>The ephemerality of hermeneutic ontology, intertwining with metacognitive dialectics, imposes a transcendent exegesis whose polysemy radiates through hermetic intertextuality, challenging immediate cognition and demanding a sophisticated semiotic decryption.</p>
+            <p>This text is working well , no remediations needed </p>
+        </main>
+    )
+}
