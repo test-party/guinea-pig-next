eaca6cfdacf5d9048b654cd4e4531ab17dbbf7b8
Artur Meinen <arturmeinen@gmail.com>
2025-03-18 14:14:53 -0400

    body tag cleanup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body aria-hidden="true">
+      <body aria-hidden="false">
         {children}
       </body>
     </html>
