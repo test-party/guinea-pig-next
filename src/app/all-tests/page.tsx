36a577971f21f32163abf747d0ffcea1f1dbf4ba
Artur Meinen <arturmeinen@gmail.com>
2025-03-18 15:47:25 -0400

    add aria-required-children

--- src/app/all-tests/page.tsx
+++ src/app/all-tests/page.tsx
@@ -295,6 +295,24 @@ export default function AllTests() {
           <div role="slider"></div>
         </div>
       </div>
+      <div>
+        <h2>ARIA-REQUIRED-CHILDREN TESTS</h2>
+        <div>
+            <div role="list">
+                <p>LIST ITEM</p>
+            </div>
+            <nav role="menu">
+
+                <span>option 1</span>
+                <span>option 2</span>
+
+                <div role="menu">
+
+                    <p>Submenu without role="menuitem"</p>
+                </div>
+            </nav>
+        </div>
+      </div>
     </div>
   );
 }
