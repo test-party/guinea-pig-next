feb0a8be77b1f2e75a4136251a3ef33a8ce0b4f4
Artur Meinen <arturmeinen@gmail.com>
2025-03-18 15:25:15 -0400

    tests

--- src/app/all-tests/page.tsx
+++ src/app/all-tests/page.tsx
@@ -240,6 +240,30 @@ export default function AllTests() {
           <p>ABC</p>
         </div>
       </div>
+      <div>
+        <h2>ARIA-INIVALID-ATTRIBUTE TESTS</h2>
+        <div>
+            <div>
+                <p aria-checked="true">ABC</p>
+            </div>
+
+            <div>
+                <p aria-checked="true">ABC</p>
+                <p aria-checked="true">ABC</p>
+
+
+            </div>
+
+            <div>
+                <button aria-expanded="true">ABC</button>
+                <button aria-expanded="false">ABC</button>
+
+            </div>
+
+            <input type="text" aria-pressed="true" />
+            <input type="text" aria-pressed="true" />
+        </div>
+      </div>
     </div>
   );
 }
