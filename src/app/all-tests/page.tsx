5a32834b92e3b8b434bb5273bfdc23870abe7a4c
Artur Meinen <arturmeinen@gmail.com>
2025-03-18 14:46:12 -0400

    add aria-deprecated-role

--- src/app/all-tests/page.tsx
+++ src/app/all-tests/page.tsx
@@ -234,6 +234,68 @@ export default function AllTests() {
           </nav>
         </div>
       </div>
+      <div>
+        <h1>ARIA-DEPRECATED-ROLE</h1>
+        <div>
+          <div role="directory">
+            <p>ABC</p>
+          </div>
+
+          <div role="directory">
+            <ul>
+              <li>Item 1</li>
+              <li>Item 2</li>
+              <li>Item 3</li>
+            </ul>
+          </div>
+
+          <div role="directory">
+            <ul>
+              <li>Parent Item 1
+                <ul>
+                  <li>Child Item 1</li>
+                  <li>Child Item 2</li>
+                </ul>
+              </li>
+              <li>Parent Item 2
+                <ul>
+                  <li>Child Item 3</li>
+                  <li>Child Item 4</li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+
+          <div role="directory" className="randomClassName">
+            <p>ABC</p>
+          </div>
+
+          <div className="randomClassName" style={{ color: 'red' }} role="directory">
+            <ul>
+              <li>Item 1</li>
+              <li>Item 2</li>
+              <li>Item 3</li>
+            </ul>
+          </div>
+
+          <div role="directory" className="randomClassName" style={{ color: 'red' }}>
+            <ul>
+              <li>Parent Item 1
+                <ul>
+                  <li>Child Item 1</li>
+                  <li>Child Item 2</li>
+                </ul>
+              </li>
+              <li>Parent Item 2
+                <ul>
+                  <li>Child Item 3</li>
+                  <li>Child Item 4</li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
