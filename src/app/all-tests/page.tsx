d5bd54b98d2d8bcdd7e22144eade2ecebfa08ce3
Artur Meinen <arturmeinen@gmail.com>
2025-03-18 15:50:18 -0400

    add-aria-require-parent

--- src/app/all-tests/page.tsx
+++ src/app/all-tests/page.tsx
@@ -298,19 +298,75 @@ export default function AllTests() {
       <div>
         <h2>ARIA-REQUIRED-CHILDREN TESTS</h2>
         <div>
-            <div role="list">
-                <p>LIST ITEM</p>
+          <div role="list">
+            <p>LIST ITEM</p>
+          </div>
+          <nav role="menu">
+
+            <span>option 1</span>
+            <span>option 2</span>
+
+            <div role="menu">
+
+              <p>Submenu without role="menuitem"</p>
             </div>
-            <nav role="menu">
-
-                <span>option 1</span>
-                <span>option 2</span>
-
-                <div role="menu">
-
-                    <p>Submenu without role="menuitem"</p>
-                </div>
-            </nav>
+          </nav>
+        </div>
+      </div>
+      <div>
+        <h2>ARIA-REQUIRED-PARENT TESTS</h2>
+        <div>
+          <div>
+            <div>
+              <div role="cell">This Cell needs row and row&apos;s parent options</div>
+            </div>
+          </div>
+          <div>
+            <div role="row">
+              <div role="cell">This Cell has row, needs row&apos;s parent options</div>
+            </div>
+          </div>
+          <div>
+            <div role="rowgroup">
+              <div>
+                <div role="cell">This Cell has rowgroup, needs row in between and rowgroup&apos;s parent options</div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div>
+              <div role="cell">This is Cell 1, it needs row and row&apos;s parent options</div>
+            </div>
+            <div>
+              <div>This is supposed to be cell under row.  It should be remediated as part of Cell 1&apos;s remediation</div>
+            </div>
+          </div>
+          <div>
+            <div role="caption">caption needs parent options</div>
+          </div>
+          <div>
+            <div role="treeitem">treeitem needs parent options</div>
+            <div>Should be treeitem</div>
+          </div>
+          <div>
+            <div role="option">option needs parent options</div>
+            <div>Should be Option</div>
+          </div>
+          <div>
+            <div role="menuitem">menuitem needs parent options</div>
+            <div>Should be menuitem</div>
+            <div>Should be menuitem</div>
+          </div>
+          <div>
+            <div role="listitem">listitem needs parent list</div>
+            <div>Should be listitem</div>
+            <div>Should be listitem</div>
+          </div>
+          <div>
+            <div role="tab">tab needs parent tablist</div>
+            <div>Should be tab</div>
+            <div>Should be tab</div>
+          </div>
         </div>
       </div>
     </div>
