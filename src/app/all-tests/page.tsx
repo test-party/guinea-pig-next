9189caa501b7fae49ad7d925bf790ee0c571ae44
Artur Meinen <arturmeinen@gmail.com>
2025-03-18 15:56:13 -0400

    add aria-toogle tests

--- src/app/all-tests/page.tsx
+++ src/app/all-tests/page.tsx
@@ -369,6 +369,32 @@ export default function AllTests() {
           </div>
         </div>
       </div>
+      <div>
+        <h2>ARIA-TOOGLE TESTS</h2>
+        <div>
+            <ul role="menu">
+                <li id="pass2" role="menuitemcheckbox" aria-checked="true">
+                </li>
+            </ul>
+            <p id="pass3Label">Sans-serif</p>
+            <ul role="menu">
+                <li id="pass3" role="menuitemradio" aria-checked="true">
+                </li>
+                <li id="pass3" role="menuitemradio" aria-checked="true">
+                </li>
+            </ul>
+            <div role="radiogroup">
+                <div id="pass4" role="radio" aria-checked="false">
+                </div>
+                <div id="pass4" role="radio" aria-checked="false">
+                </div>
+            </div>
+            <div id="fail5" role="switch" aria-checked="true">
+                <span></span>
+                <span></span>
+            </div>
+        </div>
+      </div>
     </div>
   );
 }
