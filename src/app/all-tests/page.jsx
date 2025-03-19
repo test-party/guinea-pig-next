5af9ad04c9bce2af40daff40761f6e3714bc6944
Artur Meinen <arturmeinen@gmail.com>
2025-03-19 10:31:11 -0400

    tests

--- src/app/all-tests/page.jsx
+++ src/app/all-tests/page.jsx
@@ -562,6 +562,28 @@ export default function AllTests() {
                 </div>
             </div>
       </div>
+      <div>
+        <h2>SELECT-NAME TESTS</h2>
+        <div>
+            <label>Choose a pet:</label>
+            <select name="pets" id="pet-select">
+                <option value="">--Please choose an option--</option>
+                <option value="dog">Dog</option>
+                <option value="cat">Cat</option>
+                <option value="hamster">Hamster</option>
+                <option value="parrot">Parrot</option>
+                <option value="spider">Spider</option>
+                <option value="goldfish">Goldfish</option>
+            </select>
+            <select>
+                <option value="tesla">Tesla</option>
+                <option value="ford">Ford</option>
+                <option value="chevy">Chevy</option>
+                <option value="mercedes">Mercedes</option>
+                <option value="bmw">BMW</option>
+            </select>
+        </div>
+      </div>
     </div>
   );
 }
