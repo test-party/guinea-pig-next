fa37b598e4cc4eff5c495e9ba0bf9af1c4cd4959
Artur Meinen <arturmeinen@gmail.com>
2025-03-19 10:28:19 -0400

    tests

--- src/app/all-tests/page.jsx
+++ src/app/all-tests/page.jsx
@@ -533,6 +533,35 @@ export default function AllTests() {
           <p>This text is working well , no remediations needed </p>
         </div>
       </div>
+      <div>
+        <h2>SCROLLABLE-REGION-FOCUSABLE TESTS</h2>
+        <div style={{ display: "flex", gap: "20px", padding: "20px" }}>
+                {/* Scrollable section (compliant) */}
+                <div
+                    style={{ width: "300px", height: "200px", overflow: "auto", background: "#f0f0f0", padding: "10px" }}
+                    tabIndex={0}
+                >
+                    <p><strong>Focusable Scrollable Section</strong></p>
+                    <p style={{ height: "300px" }}>This section is scrollable and accessible.</p>
+                </div>
+
+                <div style={{ width: "300px", height: "200px", overflow: "auto", background: "#dcdcdc", padding: "10px" }}>
+                    <p><strong>Non-Focusable Scrollable Section</strong></p>
+                    <p style={{ height: "300px" }}>This section is scrollable, but cannot be focused via keyboard.</p>
+                </div>
+
+                <div style={{ width: "300px", height: "200px", overflow: "auto", background: "#dcdcdc", padding: "10px" }}>
+                    <p><strong>Non-Focusable Scrollable Section</strong></p>
+                    <p style={{ height: "300px" }}>This section is scrollable, but cannot be focused via keyboard.</p>
+                </div>
+
+                <div id="pass1" style={{height: "200px", overflowY: "auto"}}>
+                    <div style={{height: "2000px"}}>
+                        <p>Content</p>
+                    </div>
+                </div>
+            </div>
+      </div>
     </div>
   );
 }
