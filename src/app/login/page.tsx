99ab17ff28763e1b80e6c7069b2c4f17b073dcfc
Artur Meinen <arturmeinen@gmail.com>
2025-04-09 11:38:13 -0300

    login path

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation'
-
+//http://localhost:3000/login
 export default function LoginPage() {
   const router = useRouter();
 
