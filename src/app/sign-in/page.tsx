958a6be664bf93b37fe404aaace515b310dd39b5
Jason Tan <jasonzhetan@yahoo.com>
2025-04-27 14:09:59 -0400

    upgrade to 14.2.5

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,5 +1,24 @@
-import { RedirectToSignIn } from "@clerk/nextjs";
+import { RedirectToSignIn, SignedIn } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
 
-export default function SignIn() {
-    return <RedirectToSignIn redirectUrl={"http://localhost:3000"} />
+export default async function SignIn() {
+    const { userId } = await auth();
+    if (!userId) {
+        return (
+            <div>
+                <RedirectToSignIn redirectUrl="/protected-link-name" />
+            </div>
+        );
+    }
+    return (
+        <div>
+            <SignedIn>
+                <h1>Welcome to the Sign In page</h1>
+                <p>You are signed in!</p>
+                <p>Your user ID is: {userId}</p>
+                <p>Click the link below to go to the protected page:</p>
+                <a href="/protected-link-name">Go to Protected Page</a>
+            </SignedIn>
+        </div>
+    );
 }
