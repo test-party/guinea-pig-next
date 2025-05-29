48066ede8bcf835de8566af0c78377fcf3680db6
alexander <alexander@testparty.ai>
2025-05-29 16:52:25 -0400

    amk - div test

--- src/app/aria-braille-equivalent/page.tsx
+++ src/app/aria-braille-equivalent/page.tsx
@@ -4,33 +4,33 @@ export default function Page() {
         <div>
             <h1>Test Page for aria-braille-equivalent Violations</h1>
 
-            <button id="pass1" aria-braillelabel="hello">Hello</button>
-            <button id="pass2" aria-braillelabel=""></button>
-            <button id="incomplete1" aria-braillelabel="hello"></button>
+            <div id="pass1" aria-braillelabel="hello">Hello</div>
+            <div id="pass2" aria-braillelabel=""></div>
+            <div id="incomplete1" aria-braillelabel="hello"></div>
 
-            <aside
+            <div
                 id="pass3"
                 aria-roledescription="table of contents"
                 aria-brailleroledescription=""
-            ></aside>
+            ></div>
 
-            <aside
+            <div
                 id="pass4"
                 aria-roledescription="table of contents"
                 aria-brailleroledescription="table of contents"
-            ></aside>
+            ></div>
 
-            <aside
+            <div
                 id="pass5"
                 aria-roledescription=""
                 aria-brailleroledescription=""
-            ></aside>
+            ></div>
 
-            <aside
+            <div
                 id="incomplete2"
                 aria-roledescription=""
                 aria-brailleroledescription="table of contents"
-            ></aside>
+            ></div>
         </div>
     );
 }
