37e347161433de3e4ddb0969042e590dfb98572c
alexander <alexander@testparty.ai>
2025-06-04 16:48:45 -0400

    amk - saving dedicated page

---
+++ src/app/document-title/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+import React from 'react';
+
+//http://localhost:3000/document-title
+const Page = () => {
+    return (
+        <div>
+            <div>Document Title Test Page</div>
+            <div>Remediation is in layout file</div>
+        </div>
+    );
+};   
+
+export default Page;
