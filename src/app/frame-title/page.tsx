875c548f65e9f14c5efeceb6d1e8006ce5fe2aa5
alexander <alexander@testparty.ai>
2025-06-18 22:58:57 -0400

    amk - frame title

---
+++ src/app/frame-title/page.tsx
@@ -0,0 +1,11 @@
+//http://localhost:3000/frame-title
+const Page = () => {
+    return (
+        <>
+            <iframe title="Passing Frame Title">Frame Title</iframe>
+            <iframe>Frame Title</iframe>
+        </>
+    )
+}
+
+export default Page
