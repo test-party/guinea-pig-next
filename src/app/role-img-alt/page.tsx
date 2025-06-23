5c919e36d798de37a999ecb2790ad4d5a4427c11
alexander <alexander@testparty.ai>
2025-06-23 14:31:30 -0400

    amk - lint cleanup

--- src/app/role-img-alt/page.tsx
+++ src/app/role-img-alt/page.tsx
@@ -6,11 +6,11 @@ const Page = () => {
                 <div role="img" aria-label="Valid Label" id="pass1"></div>
                 <div id="match">Bananas</div>
                 <div role="img" aria-labelledby="match" id="pass2"></div>
-                <div id="hidden-match" style={{display:"none"}}>Banana bombs</div>
+                <div id="hidden-match" style={{ display:'none' }}>Banana bombs</div>
                 <div role="img" aria-labelledby="hidden-match" id="pass3"></div>
                 <div role="img" title="title" id="pass4"></div>
             </div>
-            <div id="invalid-cases">    
+            <div id="invalid-cases">
                 <div role="img" id="violation1"></div>
                 <div role="img" aria-label="" id="violation2"></div>
                 <div role="img" aria-labelledby="no-match" id="violation3"></div>
