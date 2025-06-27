ef5f8d6c956624ac2155340d3f514fa87d62d119
Artur Meinen <arturmeinen@gmail.com>
2025-06-27 11:31:00 -0300

    initial changes

---
+++ src/app/valid-lang/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+//http://localhost:3000/valid-lang
+const Page = () => {
+    return (
+        <div>
+            <p lang="xxxxxxx">Esse texto esta em portugues</p>
+        </div>
+    )
+}
