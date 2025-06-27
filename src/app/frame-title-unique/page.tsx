b57b43b1d94e8407839b78db3459198d193ae7b7
Artur Meinen <arturmeinen@gmail.com>
2025-06-27 18:20:10 -0300

    add frame title unique page

---
+++ src/app/frame-title-unique/page.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default function Page() {
+    return (
+        <div>
+            <h1>Dashboard</h1>
+            <iframe src="https://calendar.google.com" width="600" height="400" />
+            <iframe
+                src="https://docs.google.com"
+                title="Document Viewer"
+                width="600"
+                height="400"
+            />
+            <iframe
+                src="https://sheets.google.com"
+                title="Document Viewer"
+                width="600"
+                height="400"
+            />
+        </div>
+    );
+}
