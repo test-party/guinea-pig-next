9dff4305cd33d43f720b756e074ca1ae08a5bff9
Artur Meinen <arturmeinen@gmail.com>
2025-06-28 18:48:34 -0300

    test

--- src/app/select-name/page.tsx
+++ src/app/select-name/page.tsx
@@ -1,29 +1,34 @@
 'use client'
-import React from 'react'
+import  React    from'react'
+
+
 
 //http://localhost:3000/select-name
-const Page = () => {
-    return (
-        <div>
-            <label>Choose a pet:</label>
-            <select name="pets" id="pet-select">
-                <option value="">--Please choose an option--</option>
-                <option value="dog">Dog</option>
-                <option value="cat">Cat</option>
-                <option value="hamster">Hamster</option>
-                <option value="parrot">Parrot</option>
-                <option value="spider">Spider</option>
-                <option value="goldfish">Goldfish</option>
-            </select>
-            <select>
-                <option value="tesla">Tesla</option>
-                <option value="ford">Ford</option>
-                <option value="chevy">Chevy</option>
-                <option value="mercedes">Mercedes</option>
-                <option value="bmw">BMW</option>
-            </select>
-        </div>
-    )
+const    Page= () =>{
+        return (
+<div>    <label>Choose a pet:</label>
+
+<select id="pet-select" name="pets">
+<option value="">--Please choose an option--</option><option value="dog">Dog</option>
+<option value="cat">Cat</option>
+<option value="hamster">Hamster</option> <option value="parrot">Parrot</option><option value="spider">
+Spider</option>
+<option value="goldfish">Goldfish</option></select>
+
+
+
+
+
+
+
+<select> <option value="tesla">Tesla</option><option value="ford">Ford</option>
+
+<option value="chevy">Chevy</option><option value="mercedes">Mercedes</option>    <option value="bmw">BMW</option>
+</select>
+</div>)
 }
 
-export default Page
+
+
+export default
+Page
