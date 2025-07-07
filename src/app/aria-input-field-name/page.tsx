3e3bd96c6b9e9036e738a4ead658a50827943f37
Artur Meinen <arturmeinen@gmail.com>
2025-07-07 12:32:38 -0300

    add aria-input-field-name

---
+++ src/app/aria-input-field-name/page.tsx
@@ -0,0 +1,8 @@
+//http://localhost:3000/aria-input-field-name
+export default function Page() {
+    return (
+        <div>
+            <div id="fail2" aria-labelledby="non-existing" role="combobox">England</div>
+        </div>
+    )
+}
