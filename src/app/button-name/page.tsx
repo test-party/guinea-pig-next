123831978fb2dc79f10976551c736687f5aa3244
Artur Meinen <arturmeinen@gmail.com>
2025-07-07 13:53:19 -0300

    fixes

--- src/app/button-name/page.tsx
+++ src/app/button-name/page.tsx
@@ -1,31 +1,9 @@
-'use client'
-import React from 'react'
-
 //http://localhost:3000/button-name
 const Page = () => {
     return (
-<div>
-    <div id="aria-deprecated-role">
-             <h2>Aria-deprecated-role</h2>
-                <p>Deprecated roles should not be used</p>
-                <div className="test-case-container">
-                    <h6>Div with depracted role "directory" 🚫</h6>
-                    <div role="directory">
-                        <p>Page content example</p>
-                    </div>
-                </div>
-            </div>
-  <p>
-                click below to subscribe!
-
-                
-            </p>
-     <button role="button">
-
-        
-     </button>
-                         </div>
+        <div>
+            <p>click below to subscribe!</p>
+            <button role="button"></button>
+        </div>
     )
 }
-
-export default Page
