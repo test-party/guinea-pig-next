ae8161f1fdc201f0f558f0e07a4a9d994fe58a88
Artur Meinen <arturmeinen@gmail.com>
2025-07-15 15:52:41 -0300

    test

--- src/app/button-name/page.tsx
+++ src/app/button-name/page.tsx
@@ -6,7 +6,9 @@ const Page = () => {
     return (
         <div>
             <p>click below to subscribe!</p>
-            <button role="button"></button>
+            <button 
+            
+            role="button"></button>
         </div>
     )
 }
