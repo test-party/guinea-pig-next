aeba8330d39aa331253a505884320a660eb09dc6
Artur Meinen <arturmeinen@gmail.com>
2025-07-23 15:29:59 -0400

    test

--- src/app/button-name/page.tsx
+++ src/app/button-name/page.tsx
@@ -8,12 +8,14 @@ const Page = () => {
     }
     return (
         <div>
+            {/* <p>dyslexia,dyslexiadyslexiadyslexiadyslexiadyslexiadyslexiadyslexiadyslexia</p>
             <p>dyslexia,dyslexiadyslexiadyslexiadyslexiadyslexiadyslexiadyslexiadyslexia</p>
             <p>dyslexia,dyslexiadyslexiadyslexiadyslexiadyslexiadyslexiadyslexiadyslexia</p>
             <p>dyslexia,dyslexiadyslexiadyslexiadyslexiadyslexiadyslexiadyslexiadyslexia</p>
             <p>dyslexia,dyslexiadyslexiadyslexiadyslexiadyslexiadyslexiadyslexiadyslexia</p>
-            <p>dyslexia,dyslexiadyslexiadyslexiadyslexiadyslexiadyslexiadyslexiadyslexia</p>
-            <button onClick={() => goToNextPage()}></button>
+            <button onClick={() => goToNextPage()}></button> */}
+            <p>Click below to subscribe</p>
+            <button id="subscribe-button"></button>
         </div>
     )
 }
