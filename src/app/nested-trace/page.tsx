2a1e7f2cf0415620c21b065e284bb949e0320e2f
alexander <alexander@testparty.ai>
2025-07-29 11:45:28 -0400

    amk - right format for root

--- src/app/nested-trace/page.tsx
+++ src/app/nested-trace/page.tsx
@@ -1,6 +1,6 @@
 //http://localhost:3000/nested-trace
 import { ChildComponent } from "./ChildComponent";
-export const RootComponent = () => {
+export default function Page() {
   // Assume instrumentation adds data-testparty="root-123" at this JSX line, recorded in allNodes with this file's URI/line/column
   return (
     <div>
