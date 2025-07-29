1ffc2f63a90ae0d288a543c26b034122aca147a6
alexander <alexander@testparty.ai>
2025-07-29 12:01:28 -0400

    amk - img update

--- src/app/nested-trace/GrandchildComponent.tsx
+++ src/app/nested-trace/GrandchildComponent.tsx
@@ -1,7 +1,7 @@
 export const GrandchildComponent = () => {
   return (
     <div>
-      <img className="object-fill w-half h-half" key='0' src={`https://picsum.photos/id/0/200/300`}></img>
+      <img className="w-[200px] h-auto object-contain" key='0' src={`https://picsum.photos/id/0/200/300`}></img>
     </div>
   );
 };
