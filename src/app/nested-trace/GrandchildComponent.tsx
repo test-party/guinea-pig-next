c51bf535b36fbf309546833d7ad79cf67a15af52
alexander <alexander@testparty.ai>
2025-07-29 11:56:18 -0400

    amk - img tage

--- src/app/nested-trace/GrandchildComponent.tsx
+++ src/app/nested-trace/GrandchildComponent.tsx
@@ -1,7 +1,7 @@
 export const GrandchildComponent = () => {
   return (
     <div>
-      <img src="example.jpg" /> {/* Empty alt text triggers axe violation */}
+      <img className="object-fill w-half h-half" key='0' src={`https://picsum.photos/id/0/200/300`}></img>
     </div>
   );
 };
